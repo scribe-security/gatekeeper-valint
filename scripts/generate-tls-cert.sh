899543c96969bf47c239bcc5b95eae4b5d530eb1
houdini91 <mdstrauss91@gmail.com>
2024-03-17 12:19:04 +0200

    try fix

--- scripts/generate-tls-cert.sh
+++ scripts/generate-tls-cert.sh
@@ -4,18 +4,13 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-determine_repo_root() {
-    if [ -n "${BASH_SOURCE[0]+x}" ]; then
-        REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
-    elif [ -n "${0}" ]; then
-        REPO_ROOT=$(dirname "${0}")/..
-    else
-        REPO_ROOT=$(pwd)
-    fi
-}
-
-# Call the function to determine REPO_ROOT
-determine_repo_root
+if [[ -v BASH_SOURCE ]]; then
+    REPO_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+elif [ -n "${0:-}" ]; then
+    REPO_ROOT=$(dirname "${0}")/..
+else
+    REPO_ROOT=$(pwd)
+fi
 
 cd "${REPO_ROOT}" || exit 1
 NAMESPACE=${NAMESPACE:-gatekeeper-valint}
