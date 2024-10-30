e1428300ee176e9fa1e68e2260ed22a463984fe8
houdini91 <mdstrauss91@gmail.com>
2024-10-30 12:45:44 +0200

    add some stress testing logic

--- scripts/stress-test.sh
+++ scripts/stress-test.sh
@@ -8,9 +8,11 @@
 TMP_DIR=".tmp"
 STRESS_PATH="stress_deployment.yaml"
 STRESS_DEPLOYMENT_PATH="$TMP_DIR/$STRESS_PATH"
+VALINT_BINARY=../valint/snapshot/main_linux_amd64_v1/valint
+list_nginx_images=( "alpine:3.18.9" "alpine:3.18.8" "alpine:3.18.7" "alpine:3.18.6" "alpine:3.18.5" "alpine:3.18.4" "alpine:3.18.4" "alpine:3.18.3" "alpine:3.18.2" "alpine:3.20.3" "alpine:3.20.2" "alpine:3.20.1" "alpine:3.19.4" "alpine:3.19.3" "alpine:3.19.2" "alpine:3.19.1" "nginx:1.21.6" "nginx:1.21.5" "nginx:1.21.3" "nginx:1.21.1" "nginx:1.26" "nginx:1.25" "nginx:1.24" "nginx:1.23" "nginx:1.22" "nginx:1.21" "nginx:1.20" "nginx:1.19" "nginx:1.18" "nginx:1.17" "nginx:1.16" "nginx:1.15" "nginx:1.14" "nginx:1.13" "nginx:1.12" "nginx:1.11" "nginx:1.10" "nginx:1.9" "nginx:1.8" "nginx:1.7")
 
 parse_args() {
-  while getopts "n:c:DxCx" arg; do
+  while getopts "n:c:DxCxSxMx" arg; do
     case "$arg" in
       n)
         echo "Test name: $OPTARG"
@@ -28,6 +30,14 @@ parse_args() {
         echo "Cleaning k8s"
         CLEAN=true
         ;;
+      M)
+        echo "Multi tag images"
+        MULTI_TAG=true
+        ;;
+      S)
+       echo "Sign images"
+       SIGN=true
+       ;;  
       *)
         echo "Invalid option: -$OPTARG" >&2
         exit 1
@@ -35,6 +45,23 @@ parse_args() {
     esac
   done
   shift $((OPTIND - 1))
+}
+
+sign_images() {
+  COUNT=$1
+  for i in $(seq 0 $COUNT); do
+    echo "Signing image: $i"
+    if [ ! -z "$MULTI_TAG" ]; then
+      docker pull ${list_nginx_images[$i]}
+      $VALINT_BINARY bom ${list_nginx_images[$i]} -o attest
+    fi
+  done
+
+  if [ -z "$MULTI_TAG" ]; then
+    docker pull nginx:latest
+    $VALINT_BINARY bom nginx:latest -o attest
+  fi
+
 }
 
 ## Create depoyment and then push to tmp.file
@@ -61,10 +88,17 @@ spec:
       containers:
 EOF
 
-  for i in $(seq 1 $COUNT); do
+  for i in $(seq 0 $COUNT); do
+
+    if [ ! -z "$MULTI_TAG" ]; then
+      IMAGE=${list_nginx_images[$i]}
+    else
+      IMAGE="nginx:latest"
+    fi
+
     cat <<EOF >> $DEP_PATH
       - name: nginx-$i
-        image: nginx:latest
+        image: $IMAGE
 EOF
     done
 }
@@ -99,4 +133,8 @@ fi
 
 if [ ! -z "$CLEAN" ]; then
     clean $STRESS_DEPLOYMENT_PATH
+fi
+
+if [ ! -z "$SIGN" ]; then
+    sign_images $IMAGE_COUNT 
 fi
